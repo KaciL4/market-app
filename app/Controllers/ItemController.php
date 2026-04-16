09cfb642fbdeb6c3f81dadf88edcd7afffdefc40
Kaci Luu <kiluu07@outlook.com>
2026-04-16 19:38:48 -0400

    add functions to itemController and  itemModel

--- app/Controllers/ItemController.php
+++ app/Controllers/ItemController.php
@@ -10,20 +10,49 @@ use DI\Container;
 
 class ItemController extends BaseController
 {
-    public function __construct(Container $container)
+    public function __construct(Container $container,private ItemModel $itemModel,private AdminModel $adminModel)
     {
         return parent::__construct($container);
     }
-    //TODO get all items or filtered by category/search -> GET /items
+    //* get all items or filtered by category/search -> GET /items
     public function index(Request $request, Response $response, array $args): Response{
+        $params = $request->getQueryParams();
+        $categoryId = isset($params['category'])? (int)$params['category']:null;
+        $search = trim($params['search']??'');
+        // fetch items by search
+        if($search!==''){
+            $items = $this->itemModel->searchItems($search);
+        }else if($categoryId){
+            $items =$this->itemModel->getItemsByCategory($categoryId);
+        }else{
+            $items=$this->itemModel->getAllItems();
+        }
 
-        
+        // fetch categories
+        $categories =$this->adminModel->getAllCategories();
+        $data=[
+            'title'=>'Items for Sale',
+            'items'=>$items,
+            'categories'=>$categories,
+            'categoryId'=>$categoryId,
+            'search'=> $search
+        ];
+
         return $this->render($response, 'items/item_list.php',$data);
     }
 
-    // TODO show a single item detail -> GET /items/{id}
+    // * show a single item detail -> GET /items/{id}
     public function show(Request $request, Response $response, array $args): Response {
         // code
+        $id =(int)$args['id'];
+        $item = $this->itemModel->getItemById($id);
+        if(!$item){
+            return $response->withHeader('Location', '/items')->withStatus(302);
+        }
+        $data=[
+            'title'=> $item['listing_product'],
+            'item'=>$item,
+        ];
 
         return $this->render($response, 'items/item_detail.php',$data);
     }
--- app/Domain/Models/itemModel.php
+++ app/Domain/Models/itemModel.php
@@ -24,8 +24,23 @@ class ItemModel extends BaseModel
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
     //TODO get item by ID
+    public function getItemById(int $id):array{
+        $sql= "SELECT i.*, c.category_name FROM items i JOIN category c ON i.category_id = c.category_id WHERE i.item_id = :id LIMIT 1";
+        $stmt= $this->pdo->prepare($sql);
+        $stmt->execute(['id'=>$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
 
-    //TODO search items by name
-
+    }
+    //*search items by name
+    public function searchItems(string $search) : array{
+        // code
+        $sql = "SELECT i.*, c.category_name FROM items i
+            JOIN category c ON i.category_id =c.category_id
+            WHERE i.status = 'Available' AND i.listing_product LIKE :search
+            ORDER BY i.listing_date DESC";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['search'=> $search]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
     //TODO get image for an item
 }
