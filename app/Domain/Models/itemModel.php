f95c5c6ef31e825c126fd868f9948cce5483e354
Kaci Luu <kiluu07@outlook.com>
2026-04-18 12:56:39 -0400

    add the item_list page that display the available items

--- app/Domain/Models/itemModel.php
+++ app/Domain/Models/itemModel.php
@@ -9,7 +9,10 @@ class ItemModel extends BaseModel
     // *get all available items with category name
     public function getAllItems():array {
         // code
-        $sql = "SELECT i.* c.category_name FROM items i JOIN category c ON i.category_id = c.category_id WHERE i status = 'Available' ORDER BY i.listing_date DESC";
+        $sql = "SELECT i.* c.category_name,u.username FROM items i
+        JOIN category c ON i.category_id = c.category_id
+        JOIN users u ON i.user_id = u.user_id
+        WHERE i status = 'Available' ORDER BY i.listing_date DESC";
 
         $stmt = $this->pdo->query($sql);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -18,14 +21,21 @@ class ItemModel extends BaseModel
     //* get items by category
         public function getItemsByCategory(int $categoryId) {
             // code
-            $sql ="SELECT i.* ,c.category_name FROM items i JOIN category c ON i.category_id = c.category_id WHERE i.status ='Available' AND i.category_id = :category_id ORDER BY i.listing_date DESC";
+            $sql ="SELECT i.* ,c.category_name,u.username FROM items i
+            JOIN category c ON i.category_id = c.category_id
+            JOIN users u ON i.user_id = u.user_id
+            WHERE i.status ='Available' AND i.category_id = :category_id
+            ORDER BY i.listing_date DESC";
             $stmt = $this->pdo->prepare($sql);
             $stmt->execute(['category_id'=> $categoryId]);
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
     //TODO get item by ID
     public function getItemById(int $id):array{
-        $sql= "SELECT i.*, c.category_name FROM items i JOIN category c ON i.category_id = c.category_id WHERE i.item_id = :id LIMIT 1";
+        $sql= "SELECT i.*, c.category_name ,u.username FROM items i
+        JOIN category c ON i.category_id = c.category_id
+        JOIN users u ON i.user_id = u.user_id
+        WHERE i.item_id = :id LIMIT 1";
         $stmt= $this->pdo->prepare($sql);
         $stmt->execute(['id'=>$id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
@@ -34,8 +44,9 @@ class ItemModel extends BaseModel
     //*search items by name
     public function searchItems(string $search) : array{
         // code
-        $sql = "SELECT i.*, c.category_name FROM items i
+        $sql = "SELECT i.*, c.category_name,u.username FROM items i
             JOIN category c ON i.category_id =c.category_id
+            JOIN users u ON i.user_id = u.user_id
             WHERE i.status = 'Available' AND i.listing_product LIKE :search
             ORDER BY i.listing_date DESC";
         $stmt = $this->pdo->prepare($sql);
--- app/Views/items/item_list.php
+++ app/Views/items/item_list.php
@@ -1,8 +1,55 @@
 <?php
 use App\Helpers\ViewHelper;
-ViewHelper::loadHeader($page_title);
+ViewHelper::loadHeader($title);
 ?>
-
+<!-- display Items  -->
+ <div class="row g-4">
+    <?php if(empty($items)): ?>
+    <div class="col-12 text-center text-muted py-5">
+        <i class="bi bi-box-seam fs-1 d-block mb-2"></i>
+                No items found.
+    </div>
+    <?php else: ?>
+        <?php foreach($items as $item): ?>
+            <div class="col-md-3 col-sm-6">
+                <div class="card h-100 border-0 shadow bg-dark text-white rounded-4 overflow-hidden item-card">
+                    <!-- Item Image (To change later) -->
+                     <img src="https://placehold.co/600x300/343a40/white?text=No+Image"
+                     class="card-img-top"
+                     alt="<?= hs($item['listing_product']) ?>"
+                     style="height: 220px;object-fit:cover;">
+                     <!-- card body of the item -->
+                      <div class="card-body d-flex flex-column px-3 pt-3 pb-4">
+                        <!-- item name -->
+                         <h5 class="fw-bold ">
+                            <?= hs($item['listing_product']) ?>
+                         </h5>
+                         <!-- description of item -->
+                          <p>
+                            <?= hs(mb_strimwidth($item['detail'],0,60,'...')) ?>
+                          </p>
+                          <p>
+                            Seller: <?= hs($item['username']) ?>
+                            </p>
+                          <!-- price  of item -->
+                           <div class="d-flex justify-content-between align-items-center mt-auto mb-3">
+                                <h5 class="text-primary fw-bold mb-0">
+                                    $<?= number_format($item['price'], 2) ?>
+                                </h5>
+                           </div>
+                           <!-- add to cart Button -->
+                            <div class="d-flex gap-2">
+                                <a href="<?= APP_BASE_URL ?>/cart/add/<?= $item['item_id'] ?>"
+                                    class="btn btn-primary flex-grow-1">
+                                    <i class="bi bi-cart-plus me-1"></i> Add to Cart
+                                </a>
+                            </div>
+                      </div>
+                </div>
+            </div>
+        <?php endforeach?>
+    <?php endif?>
+ </div>
 
 <?php
 
